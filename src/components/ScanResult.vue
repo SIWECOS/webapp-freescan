e3e0fec75f5ff947b0d3a7e00ca6e58a82cad555
David Jardin <d.jardin@djumla.de>
2018-03-08 16:10:41 +0100

    fixed arrows

--- src/components/ScanResult.vue
+++ src/components/ScanResult.vue
@@ -13,7 +13,7 @@
       <p class="scanner-check-item-description-report" v-if="scanresult.report" v-html="scanresult.report"></p>
       <div style="clear: both;"></div>
       <small><a :href="scanresult.link"
-                target="_blank">{{ $t('messages.more_info') }} &gt;&gt;</a></small>
+                target="_blank">{{ $t('messages.more_info') }}</a></small>
       <div style="clear: both;"></div>
       <ul class="scanner-check-item-details" v-show="showDetails">
         <li v-for="(detail) in scanresult.testDetails">{{ detail.name }}</li>
