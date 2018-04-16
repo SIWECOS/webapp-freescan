92f0d3d3debdce7a639d67f28a1db444f8183c70
Marcel Wege <mwege@byte5.de>
2018-04-16 08:14:10 +0200

    Hide on Error

--- src/components/ScanResult.vue
+++ src/components/ScanResult.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="scanner-check-item-data" v-if="!(scanresult.scoreTypeRaw === 'hidden' || (scanresult.scoreTypeRaw === 'bonus' && scanresult.score !== 100))">
     <div class="col-85">
-      <span class="round-check scanner-check-data" v-bind:class="{ 'score-red': scanresult.score < 30, 'score-orange': scanresult.score >= 30 && scanresult.score < 50, 'score-yellow': scanresult.score >= 50 && scanresult.score < 100, 'score-green': scanresult.score >=100 }">{{ scanresult.name}}</span>
+      <span class="round-check scanner-check-data" v-bind:class="{ 'score-red': scanresult.score < 30, 'score-orange': scanresult.score >= 30 && scanresult.score < 50, 'score-yellow': scanresult.score >= 50 && scanresult.score < 100, 'score-green': scanresult.score >=100 }" v-html="scanresult.name"></span>
     </div>
     <div class="col-20">
-      <button class="btn btn-primary" v-bind:class="(showDetails) ? 'active' : ''" v-on:click="showDetails = (showDetails) ? 0 : 1">{{ $t('messages.more_info') }}
+      <button class="btn btn-primary" v-show="!scanresult.has_error" v-bind:class="(showDetails) ? 'active' : ''" v-on:click="showDetails = (showDetails) ? 0 : 1">{{ $t('messages.more_info') }}
       </button>
     </div>
  <div style="clear: both"></div>
@@ -12,7 +12,7 @@
       <p class="scanner-check-item-description-title" v-html="scanresult.description"></p>
       <p class="scanner-check-item-description-report" v-if="scanresult.report" v-html="scanresult.report"></p>
       <div style="clear: both;"></div>
-      <small><a :href="scanresult.link"
+      <small v-show="!scanresult.has_error"><a :href="scanresult.link"
                 target="_blank">{{ $t('messages.more_info') }}</a></small>
       <div style="clear: both;"></div>
       <ul class="scanner-check-item-details" v-show="showDetails">
