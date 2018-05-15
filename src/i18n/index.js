1715f24d1954d538cdff1de38aaca69f77b917e0
David Jardin <d.jardin@djumla.de>
2018-05-15 12:14:13 +0200

    udpated i18n json styles

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -6,16 +6,16 @@ Vue.use(VueI18n)
 const messages = {
   de: {
     messages: {
-      'headline': 'SIWECOS Schnell-Check',
-      'description': '<p><b>Hinweis:</b> Nur registrierte Webseiten können auf zusätzliche kritische Schwachstellen hin geprüft werden.</p><p>Die Registrierung und Nutzung von <b>SIWECOS ist kostenlos!</b><br/>Bitte <a href="/app/#/register">registrieren Sie ihre Webseite</a> um weitere Funktionen wie den <b>täglichen Sicherheitscheck</b>, <b>automatische Benachrichtigungen</b> beim Fund einer kritische Schwachstelle und den <b>ausführlichen Sicherheitsbericht</b> für ihre Webseite nutzen zu können.</p>',
-      'field_domain': 'Domain',
-      'field_domain_placeholder': 'Domain, bspw. https://www.example.org',
-      'field_submit': 'Scan starten',
-      'could_not_start': 'Starten des Scans fehlgeschlagen, bitte versuchen Sie es später erneut',
-      'pleasewait': 'Der Scan Ihrer Domain läuft, bitte warten...',
-      'domainnotfound': 'Die Domain ist nicht über HTTPS erreichbar',
-      'registernowhead': 'Stets auf der sicheren Seite sein:',
-      'registernow': 'Registrieren Sie sich jetzt für den kostenlosen Sicherheits-Check. SIWECOS scannt Ihre Webseite täglich und informiert Sie proaktiv, wenn Sicherheitslücken auftauchen. Nur durch eine Registrierung wird Ihre Seite dauerhaft und kostenlos überwacht und Sie können im Notfall schnell reagieren. SIWECOS bietet umfangreiche Hilfestellungen und Informationen, so schützen Sie sich und Ihre Kunden.'
+      "headline": "SIWECOS Schnell-Check",
+      "description": "<p><b>Hinweis:</b> Nur registrierte Webseiten können auf zusätzliche kritische Schwachstellen hin geprüft werden.</p><p>Die Registrierung und Nutzung von <b>SIWECOS ist kostenlos!</b><br/>Bitte <a href='/app/#/register'>registrieren Sie ihre Webseite</a> um weitere Funktionen wie den <b>täglichen Sicherheitscheck</b>, <b>automatische Benachrichtigungen</b> beim Fund einer kritische Schwachstelle und den <b>ausführlichen Sicherheitsbericht</b> für ihre Webseite nutzen zu können.</p>",
+      "field_domain": "Domain",
+      "field_domain_placeholder": "Domain, bspw. https://www.example.org",
+      "field_submit": "Scan starten",
+      "could_not_start": "Starten des Scans fehlgeschlagen, bitte versuchen Sie es später erneut",
+      "pleasewait": "Der Scan Ihrer Domain läuft, bitte warten...",
+      "domainnotfound": "Die Domain ist nicht über HTTPS erreichbar",
+      "registernowhead": "Stets auf der sicheren Seite sein:",
+      "registernow": "Registrieren Sie sich jetzt für den kostenlosen Sicherheits-Check. SIWECOS scannt Ihre Webseite täglich und informiert Sie proaktiv, wenn Sicherheitslücken auftauchen. Nur durch eine Registrierung wird Ihre Seite dauerhaft und kostenlos überwacht und Sie können im Notfall schnell reagieren. SIWECOS bietet umfangreiche Hilfestellungen und Informationen, so schützen Sie sich und Ihre Kunden."
     }
   }
 }
