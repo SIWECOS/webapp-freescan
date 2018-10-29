63ea5c0fed3d886b2286f5fa8e744fee6191887f
David Jardin <d.jardin@djumla.de>
2018-10-29 11:00:41 +0100

    updated staging api urls

--- src/services/api.js
+++ src/services/api.js
@@ -5,8 +5,8 @@ axios.defaults.headers.post['Content-Type'] = 'application/json'
 export default {
   $http: axios,
   urls: {
-    start_url: 'https://ca.staging2.siwecos.de/api/v1/getFreeScanStart',
-    status_url: 'https://ca.staging2.siwecos.de/api/v1/scan/status/free/',
-    fetch_url: 'https://bla.staging2.siwecos.de/api/v1/freescan/result/'
+    start_url: 'https://ca.staging.siwecos.de/api/v1/getFreeScanStart',
+    status_url: 'https://ca.staging.siwecos.de/api/v1/scan/status/free/',
+    fetch_url: 'https://bla.staging.siwecos.de/api/v1/freescan/result/'
   }
 }
