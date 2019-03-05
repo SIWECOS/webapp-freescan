46122a19c9d448c789e337259a4d183503278ad2
David Jardin <d.jardin@djumla.de>
2019-03-05 16:22:22 +0100

    Adjust button title
    # Conflicts:
    #	src/i18n/index.js

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -17,7 +17,8 @@ const messages = {
       "domainnotfound": "Die Domain ist nicht über HTTPS erreichbar",
       "registernowhead": "Stets auf der sicheren Seite sein:",
       "lastScan": "Letzter Scan",
-      "more_info": "Mehr Informationen",
+      "more_info": "Hintergrundinformationen",
+      "domain_timedout": "Die Domain antwortet nicht oder existiert nicht",
       "detailed_info": "Ausführliche Beschreibung >>>",
       "registernow": "Registrieren Sie sich jetzt für den kostenlosen Sicherheits-Check. SIWECOS scannt Ihre Webseite täglich und informiert Sie proaktiv, wenn Sicherheitslücken auftauchen. Nur durch eine Registrierung wird Ihre Seite dauerhaft und kostenlos überwacht und Sie können im Notfall schnell reagieren. SIWECOS bietet umfangreiche Hilfestellungen und Informationen, so schützen Sie sich und Ihre Kunden."
     }
@@ -35,7 +36,8 @@ const messages = {
       "domainnotfound": "Your domain can't be reached using HTTPS",
       "registernowhead": "Stay on the safe side:",
       "lastScan": "Last Scan",
-      "more_info": "More Information",
+      "more_info": "Background Information",
+      "domain_timedout": "The domain in question is not responding or does not exist",
       "detailed_info": "Detailed Description >>>",
       "registernow": "Signup now for the free Security-Check. SIWECOS performs daily website scans and informs your proactively, when security issues are found. Only registering your site will provide constant monitoring and let you react quickly in case of upcoming issues. SIWECOS provides comprehensive help and information and let's you protect yourself and your clients."
     }
