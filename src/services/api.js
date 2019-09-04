4c1d42d81debacf9155ebd33edf49e81af87c31f
David Jardin <d.jardin@djumla.de>
2019-09-04 14:32:40 +0200

    adjust api urls

--- src/services/api.js
+++ src/services/api.js
@@ -5,8 +5,8 @@ axios.defaults.headers.post['Content-Type'] = 'application/json'
 export default {
   $http: axios,
   urls: {
-    start_url: 'https://ca.siwecos.de/api/v1/getFreeScanStart',
-    status_url: 'https://ca.siwecos.de/api/v1/scan/status/free/',
-    fetch_url: 'https://bla.siwecos.de/api/v1/freescan/result/'
+    start_url: 'https://api.siwecos.de/api/v1/getFreeScanStart',
+    status_url: 'https://api.siwecos.de/api/v1/scan/status/free/',
+    fetch_url: 'https://api.siwecos.de/api/v1/freescan/result/'
   }
 }
